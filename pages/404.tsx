f8d490ca715b107ec9766543715efce627604f4a
Mark Watney <kietdo0602@gmail.com>
2023-11-13 03:48:41 -0500

    Add error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import styles from './404.module.css';
 import Head from 'next/head';
 
-
 const NotFoundPage = () => {
-  return (
-    <div className={styles.not_found_page}>
-	  <Head>
-		  <title>404</title>
-		  <link rel='icon' href='/kd.svg' />
-	  </Head>
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist.</p>
-    </div>
-  );
+    return (
+        <div className={styles.not_found_page}>
+            <Head>
+                <title>404</title>
+                <link rel='icon' href='/kd.svg' />
+            </Head>
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you are looking for does not exist.</p>
+        </div>
+    );
 };
 
 export default NotFoundPage;
