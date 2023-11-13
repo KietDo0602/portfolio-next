52ab90c8f53b98faf99f949e4a3c621971f701e1
Mark Watney <kietdo0602@gmail.com>
2023-11-13 04:09:08 -0500

    Hosting Integration

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,6 @@ const NotFoundPage = () => {
                 <link rel='icon' href='/kd.svg' />
             </Head>
             <h1>404 - Page Not Found</h1>
-            <p>Oops! The page you are looking for does not exist.</p>
         </div>
     );
 };
