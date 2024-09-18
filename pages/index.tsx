5c729e494e7d96ea6562b1420335db6c956fa958
Mark Watney <>
2024-09-18 07:49:48 -0400

    Update page details

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,15 @@ export default function Home() {
                 <title>Kiet Do</title>
                 <link rel='icon' href='/kd.svg' />
                 <GoogleTagManager gtmId="GTM-PPRBQBLX" />
+                <meta name="description" content="I'm Kiet, Software Engineer @ Manulife." key="desc" />
+                <meta property="og:title" content="Kiet Do" />
+                <meta property="og:description" content="Welcome to my portfolio!" />
+                <meta property="og:image" content="../public/kd.svg" />
+                <meta property="og:url" content="https://www.kietdo.io/" />
+                <meta name="twitter:card" content="../public/kd.svg" />
+                <meta name="twitter:title" content="Kiet Do" />
+                <meta name="twitter:description" content="Welcome to my portfolio!" />
+                <meta name="twitter:image" content="../public/kd.svg" />
             </Head>
 
             <main>
